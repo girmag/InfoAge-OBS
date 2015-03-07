76543db6eda8d9222efe104ae12d7d838f27d518
mHailemichael <mHailemichael@mum-PC.cs.mum.edu>
2015-03-06 18:20:21 -0600

    Login and Customer Registration

--- src/com/infoage/controller/HomepageController.java
+++ src/com/infoage/controller/HomepageController.java
@@ -6,16 +6,29 @@ import java.util.List;
 
 
 
+
 import org.apache.tomcat.jni.Address;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.infoage.domain.Customer;
+import com.infoage.domain.User;
 
 @Controller
 public class HomepageController {
-	 @RequestMapping(value = {"/", "/addCustomer"})
+	@RequestMapping(value = {"/", "/login"})
+    public String inputUser(Model model) {
+	 User user=new User();
+	 model.addAttribute("user",user);
+	 
+	 
+	  return "login";
+    }
+	
+	
+	
+	 @RequestMapping(value = {"/addCustomer"})
 	    public String listBooks(Model model) {
 		 Customer newCustomer=new Customer();
 		 model.addAttribute("customer",newCustomer);
